09d3ceff30ca110ba9b5bed00252e9cbd51cebdb
Oleg Golovin <ogolovin@outlook.com>
2020-06-23 18:54:32 +0300

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # tfs-git-conventions-helper
-later
+It generates branch names and commit messages according to the convention and copies them to clipboard so you can paste it anywhere (terminal, gui)
+# Building
+```npm install```
+
+```npm run build```
+
+# Installation
+* Go to Extensions and enable 'Developer mode'
+* Unpack zip (skip if you've cloned the repo)
+* Click 'Load unpacked' and point to '/dist' folder
+* Extension should appear in the list
+![enable in chrome](/docs/enable_in_chrome1.png)
+# Usage
+* Open a workitem and find new buttons next to a title ![new buttons](/docs/usage_task1.png)
+* Clicking on the 'Commit Message' button will generate a text to the clipboard following the pattern ```#parentPbiId #taskId PbiTitleEscaped``` e.g. ```#117413 #117414 Implement all data sources for existing forms```
+* Clicking on the 'Branch Name' button will generate a text to the clipboard following the pattern ```_parentPbiId_taskId_pbiTitle``` e.g. ```_117413_Implement_all_data_sources_for_existing_forms```.  
+* Since it's being copied to the clipboard all that remains is to paste it into terminal or any gui for git.
+
+## Notes
+* It will not mention a parent item in the generated message if the workitem is not a task, may be convenient if there are no tasks in the Pbi or Bug.
+* Branch names generated don't include module prefixes so don't forget to prepend it by yourself (until implemented here).
---
+++ src/content_script.ts
@@ -0,0 +1,127 @@
+import * as $ from 'jquery';
+import { copyTextToClipboard } from './clipboardHelper';
+
+enum ItemTypes { Task = "Task", Pbi = "Product Backlog Item", Bug = "Bug" };
+enum RelTypes { Parent = "System.LinkTypes.Hierarchy-Reverse" };
+interface ItemInfoExpandedDto {
+    id: number;
+    fields: {
+        "System.WorkItemType": ItemTypes,
+        "System.Title": string
+    },
+    relations: {
+        rel: RelTypes,
+        url: string
+    }[]
+}
+
+interface ItemInfoWithParent {
+    id: number,
+    type: string,
+    title: string,
+    parent?: ItemInfoWithParent
+}
+
+(() => {
+    const config = {
+        itemContainerSelector: '.workitem-info-bar.workitem-header-bar',
+        containerClass: 'og-buttons',
+        apiUrl: window.location.origin + '/tfs/PHS-RAA/_apis/',
+        refreshTimeout: 1000 * 1
+    };
+
+    const monitorInjections = () => {
+        processItemContainers();
+        setTimeout(monitorInjections, config.refreshTimeout);
+    };
+
+    const processItemContainers = () => {
+        getContainers().forEach(processItemContainer);
+    };
+
+    const processItemContainer = async (itemContainerElement: HTMLElement) => {
+        const container = $(itemContainerElement);
+
+        if (isAlreadyInjected(container))
+            return;
+
+        // itemContainerElement.style.backgroundColor = "#5AB"; // visually marking as set
+
+        const itemLink = container.find("a").attr("href");
+        if (itemLink == null) {
+            console.log("couldn't find item element yet. Skipping...");
+            return;
+        }
+
+        const itemId = extractItemId(itemLink);
+        const itemInfo = await requestItemInfoWithParent(itemId);
+        console.log(itemInfo);
+
+        // TODO: Lost inspiration below and till the end func. Hacks, Needs rework
+        if (itemInfo.type != ItemTypes.Task) // We don't want generating for other types
+            itemInfo.parent = null;
+
+        if (isAlreadyInjected(container)) // double checking, raise conditions
+            return;
+
+        var injectionContainer = $(`<div class=${config.containerClass}></div>`);
+        $(itemContainerElement).append(injectionContainer);
+
+        createButton(injectionContainer, "Commit Message", itemInfo.parent
+            ? `#${itemInfo.parent.id} #${itemInfo.id} ${itemInfo.parent.title}`
+            : `#${itemInfo.id} ${itemInfo.title}`);
+        createButton(injectionContainer, "Branch Name", itemInfo.parent
+            ? `_${itemInfo.parent.id}_${itemInfo.id}_${escapeGitBranchName(itemInfo.parent.title)}`
+            : `_${itemInfo.id}_${escapeGitBranchName(itemInfo.title)}`);
+    };
+
+    const escapeGitBranchName = (text: string) => text.replace(/[\[?!.*\/\. ]/g, "_");
+    const isAlreadyInjected = (container) => $(container).find('.' + config.containerClass).length;
+
+    const createButton = (container, caption: string, text: string): void => {
+        var button = $(`<button>${caption}</button>`);
+        container.append(button);
+        button.on('click', () => {
+            copyTextToClipboard(text, container.get(0));
+            console.log("copied " + text);
+        });
+    }
+
+    const requestItemInfoWithParent = async (id: number, withParent = true): Promise<ItemInfoWithParent> => {
+        const itemUrl = composeExpandedItemUrl(id);
+        const info: ItemInfoExpandedDto = await fetch(itemUrl).then(response => response.json());
+        let result: ItemInfoWithParent = fromDto(info);
+
+        if (!withParent)
+            return result;
+
+        const parentRelations = info.relations.filter(item => item.rel === RelTypes.Parent);
+        if (!parentRelations.length) {
+            console.warn(`No parent for the item with Id ${id} found`);
+            return result;
+        }
+
+        const parentItemUrl = parentRelations[0].url; // not expanded
+        const parentInfo: ItemInfoExpandedDto = await fetch(parentItemUrl).then(response => response.json());
+        result = { ...result, parent: fromDto(parentInfo) };
+
+        return result;
+    }
+
+    const fromDto = (info: ItemInfoExpandedDto) => {
+        return {
+            id: info.id,
+            type: info.fields["System.WorkItemType"],
+            title: info.fields["System.Title"],
+        };
+    }
+
+    const composeItemUrl = (id: number): string => `${config.apiUrl}wit/workitems/${id}`;
+    const composeExpandedItemUrl = (id: number): string => `${composeItemUrl(id)}?$expand=relations`;
+    const getContainers = () => document.querySelectorAll(config.itemContainerSelector);
+    const extractItemId = (url: string): number => +url.substring(url.lastIndexOf('/') + 1); // TODO: probably needs regex parsing for safety
+
+
+    monitorInjections();
+})();
+
